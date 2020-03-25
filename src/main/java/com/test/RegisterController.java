beef67db7b822b215092b109c48bda97e3024b27
Furkan Kayar <furkankayar427@hotmail.com>
2020-03-25 20:40:14 +0300

    Registration system has been updated as asynchronous task

--- src/main/java/com/test/RegisterController.java
+++ src/main/java/com/test/RegisterController.java
@@ -4,31 +4,25 @@ import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
+import javafx.concurrent.WorkerStateEvent;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
 import javafx.util.Duration;
 
-import java.io.UnsupportedEncodingException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 
 public class RegisterController{
 
@@ -36,41 +30,49 @@ public class RegisterController{
     @FXML private JFXTextField registerSceneEmailTextField;
     @FXML private JFXPasswordField registerScenePasswordField;
     @FXML private JFXButton registerSceneRegisterButton;
+    @FXML private Label registerStatusLabel;
     @FXML private StackPane parentContainer;
     @FXML private VBox mainVbox;
 
-    private final CloseableHttpClient httpClient = HttpClients.createDefault();
 
     @FXML
     public void register(){
 
-        registerSceneRegisterButton.setDisable(true);
-
-        HttpPost request = new HttpPost(MainApp.baseURL + "/auth/register");
-        request.addHeader("Content-Type", "application/json");
+        String username = registerSceneUsernameTextField.getText();
+        String email = registerSceneEmailTextField.getText();
+        String password = registerScenePasswordField.getText();
         
-        JsonObject requestBody = new JsonObject();
-        requestBody.addProperty("username", registerSceneUsernameTextField.getText());
-        requestBody.addProperty("email", registerSceneEmailTextField.getText());
-        requestBody.addProperty("password", registerScenePasswordField.getText());
-        
-        try{
-            request.setEntity(new StringEntity(requestBody.toString()));
-            HttpResponse response = httpClient.execute(request);
-            JsonObject responseBody = JsonParser.parseString(EntityUtils.toString(response.getEntity(), "UTF-8")).getAsJsonObject();
-            
-            if(responseBody.get("success") != null && responseBody.get("success").getAsBoolean() == true){
-                System.out.println(responseBody.get("message").getAsString());   
-            }
-            else{
-                System.out.println(responseBody);
-            }
-        }
-        catch(Exception ex){
-            System.out.println(ex.getMessage());
-        }
 
-        registerSceneRegisterButton.setDisable(false);
+        final AsyncRegistration asyncRegistration = new AsyncRegistration(username, email, password);
+        //registerStatusLabel.textProperty().bind(asyncRegistration.messageProperty());
+
+        asyncRegistration.setOnRunning(new EventHandler<WorkerStateEvent>() {
+
+            public void handle(WorkerStateEvent event) {
+                registerSceneRegisterButton.setDisable(true);
+            }            
+        });
+
+        asyncRegistration.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+            public void handle(WorkerStateEvent event) {
+                if((Boolean)asyncRegistration.getValue().get(0) == true){
+                    registerStatusLabel.setText(asyncRegistration.getValue().get(1).toString());
+                    registerStatusLabel.setTextFill(Paint.valueOf("#25BF1A"));
+                    registerStatusLabel.setVisible(true);
+                }
+                else{
+                    registerStatusLabel.setText(asyncRegistration.getValue().get(1).toString());
+                    registerStatusLabel.setTextFill(Paint.valueOf("#DD2B2B"));
+                    registerStatusLabel.setVisible(true);
+                }
+                registerSceneRegisterButton.setDisable(false);
+            }
+        });
+
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        executorService.execute(asyncRegistration);
+        executorService.shutdown();
+
     }
 
     @FXML
